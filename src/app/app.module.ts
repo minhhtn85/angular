aae8632f91a43f001d3ea7af784dad5092d654a7
minh hoang <minhhtn@gmail.com>
2018-07-11 03:39:42 +0000

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module'; // <-- NgModel lives here
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
